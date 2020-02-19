d852fc2715abd76c819d93806d2fbfc5f4f5720e
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-02-19 21:43:12 +0100

    last update

--- js/index.js
+++ js/index.js
@@ -5,21 +5,23 @@ $(document).ready(function(){
 		$('#movie-box').append(`
 
 
-		<div class="col-sm-6 col-md-5 col-12 bg-dark my-1 marign" id="items${movieData.movies[i].id}">
+		<div class ="col-sm-5 col-md-5 bg-dark my-1 back" id="items${movieData.movies[i].id}">
 			<div class= "row">
 				<div class=" col-sm-6 col">
 				<img src="${movieData.movies[i].image}" class= " w-100 rounded float-left m-1" alt=".">
 				</div> 
-				<div class=" col-sm-6  text-light text-center ">
+				<div class=" col-sm-6  text-light text-center">
 					<h3 >${movieData.movies[i].name}</h3>
 					
 					<div class="container">
 					<img id="artist_img" src="${movieData.movies[i].artist_img}" class= "w-50" alt=".">
 					<h4>${movieData.movies[i].artist_name}</h4>
 							<div class="col-sm-10  my-2 ml-auto">
-							<div class="row ">
-							<button id="likeButton" type="button" class="btn btn-light " name="blabutton" >Like</button>
-							<p id="bo" class="bo"></p>
+							<div class="row justify-content-around">
+							<button id="likeButton" type="button" class="btn button " name="blabutton" >Like</button>
+							<div class="likecounter">
+							<p id="bo" class="  m-2  bo"></p>
+							</div>
 							</div>
 							</div>
 
@@ -28,6 +30,7 @@ $(document).ready(function(){
 		</div>
 	
 		`);
+
 
 
 
@@ -46,18 +49,27 @@ for (var y = 0; y < button.length; y ++){
 var count = 0;
 	
 function likeIt(){
- t = count += 1;
-	console.log(t)
 
-document.getElementsByName("bo").innerHTML = t;
-
+  var $count = $(this).parent().find('.bo');
+  $count.html($count.html() * 1 + 1);
 }
-
-
-
 
 });
 
+
+
+document.getElementById("sortButton").addEventListener("click",port);
+var output = document.getElementsByName("bo");
+
+function port() {
+
+// for (var i = 0; i < movieData.movies.length; i ++){
+
+var putout = movieData.movies.sort(function(a, b){return a.output - b.output});
+// }
+console.log(output);
+
+}
 
 
 
